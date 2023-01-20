e7673313866b8650a8d9bd708382bbaccd66b605
Shaun Wu <shaunwu2001@gmail.com>
2023-01-20 12:45:34 -0500

    Classify HDL results in qualitative terms

--- blood_calculator.py
+++ blood_calculator.py
@@ -21,5 +21,14 @@ def HDL_input():
     HDL_value = input("Enter the HDL Result:")
     HDL_value = int(HDL_value)
     return HDL_value
+
+def HDL_analysis(HDL_int):
+    if HDL_int >= 60:
+        answer = "Normal"
+    elif 40 <= HDL_int < 60:
+        answer = "Borderline Low"
+    else:
+        answer = "Low"
+    return answer
     
 interface()
